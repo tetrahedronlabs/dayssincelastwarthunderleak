b43538fcab63eb326cd77cc34741d7358967fb04
Brandon <brandnlholl@gmail.com>
2024-05-09 20:57:39 -0700

    Add cheerio dependency and update check route

--- app/api/check/route.ts
+++ app/api/check/route.ts
@@ -1,12 +1,21 @@
-import type { NextRequest } from 'next/server';
- 
-export function GET(request: NextRequest) {
-  const authHeader = request.headers.get('authorization');
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response('Unauthorized', {
+import type { NextRequest } from "next/server";
+import * as cheerio from "cheerio";
+
+export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get("authorization");
+  if (
+    process.env.NODE_ENV !== "development" &&
+    authHeader !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return new Response("Unauthorized", {
       status: 401,
     });
   }
- 
+  const response = await fetch("https://en.wikipedia.org/wiki/War_Thunder");
+  const html = await response.text();
+  const $ = cheerio.load(html);
+  const data = $('.red').text();
+  console.log(data);
+
   return Response.json({ success: true });
 }

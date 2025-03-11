c674b3c3d22c177c8238df8be406b5b0d227f355
Brandon <brandnlholl@gmail.com>
2025-03-11 01:41:20 -0700

    Migrate to cloudflare workers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
+import localFont from "next/font/local";
 import "./globals.css";
 import Script from "next/script";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export const metadata: Metadata = {
   title: "Days Since the Last War Thunder Leak",
@@ -20,7 +31,11 @@ export default function RootLayout({
         src="https://analytics.tetrahedron.dev/script.js"
         data-website-id="7bbf36ec-2c73-405e-8c0f-8ef83d7208cb"
       />
-      <body className={GeistSans.className}>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,34 @@
-export const dynamic = "force-dynamic";
-import { get } from "@vercel/edge-config";
+export const dynamic = 'force-dynamic'
+import { getCloudflareContext } from "@opennextjs/cloudflare";
 import Link from "next/link";
 
 export default async function Home() {
-  const latestDate = await get("latestDate");
+  let daysSinceLastLeak = 0;
+
+  try {
+    const db = getCloudflareContext()?.env?.database;
+    if (db) {
+      const date = await db.get("latestDate");
+      if (date) {
+        daysSinceLastLeak = Math.floor(
+          (new Date().getTime() - new Date(date as string).getTime()) /
+            (1000 * 60 * 60 * 24)
+        );
+      }
+    }
+  } catch (error) {
+    console.error("Error accessing database:", error);
+    daysSinceLastLeak = 10;
+  }
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-zinc-900 font-mono text-white">
-      <div className="text-9xl font-extrabold">
-        {Math.floor(
-          (new Date().getTime() - new Date(latestDate as number).getTime()) /
-            (1000 * 60 * 60 * 24)
-        )}
-      </div>
+      <div className="text-9xl font-extrabold">{daysSinceLastLeak}</div>
       <div className="mt-4 text-gray-400">
         Days since the last War Thunder leak
       </div>
       <div className="absolute top-4 right-4 text-sm text-zinc-400">
-        Last checked: {new Date().getMinutes()} minutes ago
+        Last checked: {Math.floor(new Date().getMinutes() / 5)} minutes ago
       </div>
       <div className="absolute bottom-4 left-4 text-sm text-zinc-500">
         <p>
